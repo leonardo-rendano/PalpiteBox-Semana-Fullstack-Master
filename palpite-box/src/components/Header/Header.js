47dc04dac56a188c96c79712489bfcc6e44ccefb
Leonardo-Rendano <62267208+Leonardo-Rendano@users.noreply.github.com>
2020-07-03 02:28:32 -0300

    Inclusão do link para a home na logo

--- palpite-box/src/components/Header/Header.js
+++ palpite-box/src/components/Header/Header.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 import Logo from '../../assets/images/logo_palpitebox.png';
 
 export default function Header() {
@@ -7,7 +9,9 @@ export default function Header() {
     <>
       <header className='bg-teal-600 p-4 shadow-md'>
         <div className='container mx-auto'>
-          <img className='mx-auto w-40' src={Logo} alt="PalpiteBox"/>
+          <Link to="/Main">
+            <img className='mx-auto w-40' src={Logo} alt="PalpiteBox"/>
+          </Link>
         </div>
       </header>
     </>
